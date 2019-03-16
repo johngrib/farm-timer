e07958d7bb090c32b984c2b983944fa9cbfea13d
John Grib <johngrib82@gmail.com>
2019-03-16 18:54:55 +0900

    Enable stop button
    이제부터 stop/start 버튼의 사용이 가능합니다.
    이전 작업에서는 시간의 흐름에 따른 tick을 계속해서
    elapsed에 더하는 방법으로 elapsed를 표현했습니다.
    그러나 이 작업부터는 정확도를 위해 다음의 방법을 사용합니다.
    
    * start 버튼을 누르면
        * 현재의 timestamp를 started로 기록합니다.
        * elapsed에 save 값을 덮어씌웁니다.
    * stop 버튼을 누르면
        * save에 현재의 timestamp - started 값을 기록합니다.
    * 화면에 보여주는 elapsed를 계산하는 공식은 다음과 같습니다.
        * 현재의 timestamp - started + save

--- web/js/main.js
+++ web/js/main.js
@@ -10,16 +10,36 @@ Vue.component('timer-list', {
             return time.getUTCHours() + ':' + time.getUTCMinutes() + ':' + time.getUTCSeconds();
         }
     },
-    methods: {}
+    methods: {
+        toggle: function(state) {
+            state.on = !state.on;
+
+            if (state.on) {
+                console.log('start pressed')
+                state.started = new Date().getTime();
+                state.elapsed = state.save;
+            } else {
+                console.log('stop pressed')
+                state.save += new Date().getTime() - state.started;
+            }
+        },
+        getElapsed: function(state) {
+            return new Date().getTime() - state.started + state.save;
+        }
+    }
 })
 
 var timers = [{
     name: '운동',
-    on: true,
+    on: false,
+    save: 0,
+    started: 0,
     elapsed: 0,
 }, {
     name: '공부',
     on: false,
+    save: 0,
+    started: 0,
     elapsed: 0,
 }];
 
@@ -35,7 +55,9 @@ const tick = 100;
 
 setTimeout(function upd() {
     timers = timers.map(function(item) {
-        item.elapsed += tick;
+        if (item.on) {
+            item.elapsed = new Date().getTime() - item.started + item.save;
+        }
         return item;
     });
     setTimeout(upd, tick)
