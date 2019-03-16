10966ff185a884ac5ab51d72b5c0614785d5ae57
rollmind <eyerama@gmail.com>
2019-03-16 21:46:14 +0900

    Change name FarmTimerTests.swift to TimerServiceTests for testing TimerService Architect TimerService with protocols - TimeIntervalContainable     - 시간 넘버 타입인 TimeInterval 타입의 변수 time을 정의 - TimerContainable     - DispatchSourceTimer 변수를 정의합니다.     - 타이머의 상태값에 대한 변수를 정의합니다. *DispatchSourceTimer 자신은 자신의 상태를 가지지 않기 때문에 별도로 정의합니다. - TimerState     - 타이머의 상태값을 정의합니다. (empty, started, paused) - TimerProcessable     - 타이머의 실행, 정지, 재설정과 관련한 함수들을 정의합니다.

---
+++ ios/FarmTimer/FarmTimerTests/TimerServiceTests.swift
@@ -0,0 +1,69 @@
+//
+//  FarmTimerTests.swift
+//  FarmTimerTests
+//
+//  Created by eyetin on 14/03/2019.
+//  Copyright © 2019 rollmind. All rights reserved.
+//
+
+import XCTest
+@testable import FarmTimer
+
+class TimerServiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testTimerService() {
+        
+    }
+    
+}
+
+protocol TimeIntervalContainable {
+    
+    var time: TimeInterval { get }
+    
+}
+
+enum TimerState {
+    
+    case empty // 타이머를 생성하지 않은 상태
+    case started // 타이머를 실행한 상태
+    case paused // 타이머를 정지한 상태
+    
+}
+
+protocol TimerContainable {
+    
+    var timer: DispatchSourceTimer? { get }
+    var timerState: TimerState { get }
+    
+}
+
+protocol TimerProcessable {
+    
+    /// 타이머를 실행합니다. 타이머를 생성하기 전이라면 타이머를 생성하고, 정지한 상태라면 다시 실행하고, 이미 실행한 상태라면 아무런 작업을 하지 않습니다.
+    func start()
+    
+    /// 타이머를 멈춥니다. 이미 멈춘 상태라면 무시합니다.
+    func pause()
+    
+    /// 타이머를 정지하고, 타이머 인스턴스를 메모리에서 제거합니다. 이미 정지한 상태라면 제거만 합니다.
+    func clear()
+    
+}
+
+extension TimerProcessable where Self: TimerContainable {
+    
+    /// 타이머를 생성하고 반환합니다.
+    func createTimer() -> DispatchSourceTimer {
+        return DispatchSource.makeTimerSource()
+    }
+    
+}
