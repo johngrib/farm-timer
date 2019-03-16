4a3ee313abbc34697899212da27deeb616229c4e
John Grib <johngrib82@gmail.com>
2019-03-16 13:03:53 +0900

    Make time flow visible
    DOM manipulation was inconvenient, so I inserted vue.js.

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,42 @@
-import {
-    add
-} from './timer.mjs';
+Vue.component('timer-list', {
+    template: '#timer-template',
+    props: {
+        timers: Array,
+    },
+    computed: {},
+    filters: {
+        toTimeFormat: function(timestamp) {
+            const time = new Date(timestamp);
+            return time.getUTCHours() + ':' + time.getUTCMinutes() + ':' + time.getUTCSeconds();
+        }
+    },
+    methods: {}
+})
 
-console.log(add(1, 41));
+var timers = [{
+    name: '운동',
+    on: true,
+    elapsed: 0,
+}, {
+    name: '공부',
+    on: false,
+    elapsed: 0,
+}];
+
+// bootstrap the demo
+var demo = new Vue({
+    el: '#demo',
+    data: {
+        timers
+    }
+});
+
+const tick = 100;
+
+setTimeout(function upd() {
+    timers = timers.map(function(item) {
+        item.elapsed += tick;
+        return item;
+    });
+    setTimeout(upd, tick)
+}, tick);
