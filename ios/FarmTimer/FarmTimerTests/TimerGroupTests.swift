e6ad66ac6012245d79b1a42befd410d13521b378
rollmind <eyerama@gmail.com>
2019-03-24 23:10:02 +0900

    불필요한 UI 테스트 타겟을 제거했습니다.

---
+++ ios/FarmTimer/FarmTimerTests/TimerGroupTests.swift
@@ -0,0 +1,28 @@
+//
+//  TimerGroupTests.swift
+//  FarmTimerTests
+//
+//  Created by eyemac on 24/03/2019.
+//  Copyright © 2019 rollmind. All rights reserved.
+//
+
+import XCTest
+@testable import FarmTimer
+
+class TimerGroupTests: XCTestCase {
+
+    var group: TimerGroup!
+    var groupAdd: TimerGroupAddable!
+
+    override func setUp() {
+        group = TimerGroup()
+        groupAdd = group
+    }
+    
+    func testTimerGroupAdd() {
+        
+        
+        
+    }
+
+}
