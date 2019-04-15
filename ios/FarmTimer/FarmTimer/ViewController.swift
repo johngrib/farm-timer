6f9248e451d881c84eafcffb89d5c499fa2a96fa
rollmind <eyerama@gmail.com>
2019-04-15 21:15:12 +0900

    Add new FarmTimer ios Project

---
+++ ios/FarmTimer/FarmTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FarmTimer
+//
+//  Created by eyemac on 15/04/2019.
+//  Copyright © 2019 rollmind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
