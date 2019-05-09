934236dfec5adf85befec45a52e84d6613697cfe
rollmind <eyerama@gmail.com>
2019-05-09 22:00:59 +0900

    Add insert Time functionality Add delete Time functionality

--- ios/FarmTimer/FarmTimer/ViewController.swift
+++ ios/FarmTimer/FarmTimer/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UITableViewController {
 
     var context: NSManagedObjectContext!
 
-    lazy var fetchRequest: NSFetchRequest<Time> = Time.fetchRequest()
+    lazy var fetchRequest: NSFetchRequest<Time> = {
+        let request: NSFetchRequest<Time> = Time.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "createdate", ascending: true)]
+        return request
+    }()
     lazy var fetchedResult: NSFetchedResultsController = NSFetchedResultsController(fetchRequest: self.fetchRequest, managedObjectContext: self.context, sectionNameKeyPath: nil, cacheName: "com.rollmind.farmtimer.times")
 
     override func viewDidLoad() {
@@ -24,6 +28,9 @@ class ViewController: UITableViewController {
 
         fetchedResult.delegate = self
         try! fetchedResult.performFetch()
+
+        tableView.tableFooterView = UIView()
+        tableView.reloadData()
     }
 
 }
@@ -31,7 +38,7 @@ class ViewController: UITableViewController {
 extension ViewController {
 
     @objc func addTime(_ sender: Any) {
-        let alert = UIAlertController(title: "Add Time", message: nil, preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "Add Time", message: nil, preferredStyle: .alert)
         alert.addTextField { textField in
             textField.placeholder = "Insert timer label"
         }
@@ -39,6 +46,7 @@ extension ViewController {
         alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { _ in
             self.addTime(title: alert.textFields?[0].text)
         }))
+        present(alert, animated: true, completion: nil)
     }
 
     func addTime(title: String?) {
@@ -46,7 +54,8 @@ extension ViewController {
         let time = Time(context: context)
         time.label = title
         time.time = 0
-        context.insert(time)
+        time.createdate = Date()
+        fetchedResult.managedObjectContext.insert(time)
     }
 
 }
@@ -65,6 +74,19 @@ extension ViewController {
         return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
     }
 
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            let time = fetchedResult.object(at: indexPath)
+            fetchedResult.managedObjectContext.delete(time)
+        default: break
+        }
+    }
+
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if let cell = cell as? TimeCell {
             let time = fetchedResult.object(at: indexPath)
@@ -77,6 +99,29 @@ extension ViewController {
 
 extension ViewController: NSFetchedResultsControllerDelegate {
 
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
 
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        let olds: [IndexPath] = indexPath.flatMap({ [$0] }) ?? []
+        let news: [IndexPath] = newIndexPath.flatMap({ [$0] }) ?? []
+        switch type {
+        case .insert:
+            tableView.insertRows(at: news, with: .fade)
+        case .delete:
+            tableView.deleteRows(at: olds, with: .fade)
+        case .update:
+            tableView.reloadRows(at: olds, with: .none)
+        case .move:
+            tableView.moveRow(at: olds[0], to: news[0])
+        @unknown default:
+            break
+        }
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
 
 }
